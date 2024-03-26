10113082a0a85b655a64c17d51a3eb6b112faea9
Filipemtb <filipera3@gmail.com>
2024-03-26 06:39:15 -0300

    create: add UI class to print the board

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Olá mundo!");
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
