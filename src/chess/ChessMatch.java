57fbeee54ada0bd7277aa23f2850d3b138f89f51
Filipemtb <filipera3@gmail.com>
2024-03-27 11:32:23 -0300

    feat: add methods to verify if exists possible moves from piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -39,6 +39,9 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There isn´t piece on source position");
         }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("There isn´t possible moves for the chosen piece!");
+        }
     }
 
     private Piece makeMove(Position source, Position target) {
