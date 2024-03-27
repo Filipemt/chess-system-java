0458a172c20eddc93e58bb0f13fe0ca4fbcca7c1
Filipemtb <filipera3@gmail.com>
2024-03-27 12:03:34 -0300

    feat: add methods to validate target position

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,6 +31,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
@@ -42,6 +43,12 @@ public class ChessMatch {
         if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There isn´t possible moves for the chosen piece!");
         }
+    }
+
+    private void  validateTargetPosition(Position source, Position target) {
+            if (!board.piece(source).possibleMove(target)) {
+                throw new ChessException("The chosen piece can´t move to target position!");
+            }
     }
 
     private Piece makeMove(Position source, Position target) {
