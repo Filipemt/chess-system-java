0b1dbe655b641d35a9622e03b21e75076fb2364f
Filipemtb <filipera3@gmail.com>
2024-04-08 14:53:15 -0300

    feat: "add method to show current player"

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+    private Integer turn;
     private Board board;
+    private Color currentPlayer;
 
     public ChessMatch() {
         board = new Board(8,8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+
+    public Integer getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public ChessPiece[][] getPieces() {
@@ -39,6 +51,8 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
+
         return (ChessPiece) capturedPiece;
     }
 
@@ -46,6 +60,11 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There isn´t piece on source position");
         }
+
+        if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+            throw new ChessException("The chosen piece isn´t yours, choose your piece!");
+        }
+
         if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There isn´t possible moves for the chosen piece!");
         }
@@ -55,6 +74,11 @@ public class ChessMatch {
             if (!board.piece(source).possibleMove(target)) {
                 throw new ChessException("The chosen piece can´t move to target position!");
             }
+    }
+
+    private void nextTurn() {
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
 
     private Piece makeMove(Position source, Position target) {
