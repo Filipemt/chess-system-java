ee8204354f0d1fd7ade733ce51be661cade66704
Filipemtb <filipera3@gmail.com>
2024-04-09 11:10:26 -0300

    feat: add check-mate logic

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -48,5 +48,8 @@ public class Main {
                 sc.nextLine();
             }
         }
+
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
